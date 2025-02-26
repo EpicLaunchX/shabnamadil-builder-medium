ff31d487adfc01d1f18b8c584c37f3b5ef6f4665
shabnamadil <sebnemadil1999@gmail.com>
2025-02-26 20:36:48 +0400

    fix: change syntax

--- src/pytemplate/domain/validators.py
+++ src/pytemplate/domain/validators.py
@@ -1,24 +1,24 @@
-from marshmallow import fields, Schema, validates, ValidationError
+import marshmallow
 
 
-class BurgerSchema(Schema):
-    bread = fields.String(required=True)
-    patty = fields.String(required=True)
+class BurgerSchema(marshmallow.Schema):
+    bread = marshmallow.fields.String(required=True)
+    patty = marshmallow.fields.String(required=True)
 
-    @validates("bread")
+    @marshmallow.validates("bread")
     def validate_bread(self, value):
         if value is None or value == "":
-            raise ValidationError("Bread field is required")
+            raise marshmallow.ValidationError("Bread field is required")
 
-    @validates("patty")
+    @marshmallow.validates("patty")
     def validate_patty(self, value):
         if value is None or value == "":
-            raise ValidationError("Patty field is required")
+            raise marshmallow.ValidationError("Patty field is required")
 
 
 def burger_factory(data):
     try:
         validated_data = BurgerSchema().load(data)
         return validated_data
-    except ValidationError as err:
+    except marshmallow.ValidationError as err:
         return {"errors": err.messages}
