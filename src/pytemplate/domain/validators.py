4b07daf2e31d4d0455ebfb93b9023fe22a56d24b
shabnamadil <sebnemadil1999@gmail.com>
2025-02-26 20:22:55 +0400

    fix: add burger_factory

--- src/pytemplate/domain/validators.py
+++ src/pytemplate/domain/validators.py
@@ -8,14 +8,22 @@ class BurgerSchema(Schema):
     @validates("bread")
     def validate_bread(self, value):
         if not isinstance(value, str):
-            raise ValidationError("This field must be a string")
+            raise ValidationError("Bread field must be a string")
 
         if value is None or value == "":
-            raise ValidationError("This field is required")
+            raise ValidationError("Bread field is required")
 
     @validates("patty")
     def validate_patty(self, value):
         if not isinstance(value, str):
-            raise ValidationError("This field must be a string")
+            raise ValidationError("Bread field must be a string")
         if value is None or value == "":
-            raise ValidationError("This field is required")
+            raise ValidationError("Bread field is required")
+
+
+def burger_factory(data):
+    try:
+        validated_data = BurgerSchema().load(data)
+        return validated_data
+    except ValidationError as err:
+        return {"errors": err.messages}
