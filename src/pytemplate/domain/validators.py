3fb5b415cfa96e1ca26e862085c52d3c448462e7
shabnamadil <sebnemadil1999@gmail.com>
2025-02-26 20:13:36 +0400

    fix: change validation type

--- src/pytemplate/domain/validators.py
+++ src/pytemplate/domain/validators.py
@@ -1,24 +1,21 @@
-from marshmallow import fields, Schema, ValidationError
-
-
-def validate_field_type(value):
-    if not isinstance(value, str):
-        raise ValidationError("This field must be a string")
-
-
-def validate_required(value):
-    if value is None or value == "":
-        raise ValidationError("This field is required")
+from marshmallow import fields, Schema, validates, ValidationError
 
 
 class BurgerSchema(Schema):
-    bread = fields.String(required=True, validate=[validate_field_type, validate_required])
-    patty = fields.String(required=True, validate=[validate_field_type, validate_required])
+    bread = fields.String(required=True)
+    patty = fields.String(required=True)
 
+    @validates("bread")
+    def validate_bread(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("This field must be a string")
 
-def burger_factory(data):
-    try:
-        validated_data = BurgerSchema().load(data)
-        return validated_data
-    except ValidationError as err:
-        return {"errors": err.messages}
+        if value is None or value == "":
+            raise ValidationError("This field is required")
+
+    @validates("patty")
+    def validate_patty(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("This field must be a string")
+        if value is None or value == "":
+            raise ValidationError("This field is required")
